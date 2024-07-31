b4400174e5b4133cfdd2c808222d107686633fcb
mikey <mforest@protonmail.com>
2024-07-31 18:28:56 -0400

    style: fixing some styles

--- components/InstructorList.vue
+++ components/InstructorList.vue
@@ -3,8 +3,8 @@
     <ion-grid>
       <div v-if="filteredInstructors.length > 0">
         <transition-group name="fade" tag="ion-row">
-          <ion-col size="12" size-md="6" v-for="(instructor, index) in filteredInstructors" :key="index">
-            <ion-card>
+          <ion-col size="12" size-lg="6" v-for="(instructor, index) in filteredInstructors" :key="index">
+            <ion-card class="m-2">
               <NuxtLink prefetch :to="`/instructors/${instructor.slug}`">
                 <div class="flex flex-row items-center space-x-4">
                   <!-- Instructor Image with conditional rendering and rounded-full for round images -->
@@ -69,20 +69,20 @@ const filteredInstructors = computed(() => {
 </script>
 <style scoped>
   ion-grid {
-    --ion-grid-padding: 5px;
+    --ion-grid-padding: 0px;
 
-    --ion-grid-padding-xs: 5px;
-    --ion-grid-padding-sm: 5px;
-    --ion-grid-padding-md: 5px;
-    --ion-grid-padding-lg: 5px;
-    --ion-grid-padding-xl: 5px;
+    --ion-grid-padding-xs: 0px;
+    --ion-grid-padding-sm: 0px;
+    --ion-grid-padding-md: 0px;
+    --ion-grid-padding-lg: 0px;
+    --ion-grid-padding-xl: 0px;
 
-    --ion-grid-column-padding: 10px;
+    --ion-grid-column-padding: 5px;
 
-    --ion-grid-column-padding-xs: 10px;
-    --ion-grid-column-padding-sm: 10px;
-    --ion-grid-column-padding-md: 10px;
-    --ion-grid-column-padding-lg: 10px;
-    --ion-grid-column-padding-xl: 10px;
+    --ion-grid-column-padding-xs: 5px;
+    --ion-grid-column-padding-sm: 5px;
+    --ion-grid-column-padding-md: 5px;
+    --ion-grid-column-padding-lg: 5px;
+    --ion-grid-column-padding-xl: 5px;
   }
 </style>
