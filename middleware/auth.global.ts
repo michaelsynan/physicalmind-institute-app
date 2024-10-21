4bafac82a9476ce6f89bd0d5a330f7e9014a8c78
mikey <mforest@protonmail.com>
2024-10-21 10:58:59 -0400

    feat: fixing middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,5 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const user = useSupabaseUser();
+  console.log("middleware wokring!");
   if (!user.value && to.path !== "/login") {
     return navigateTo("/login");
   }
